9119ad5110c410e71e08cc2158a27c3a76a206fe
ZerxesWolfHawk <46700812+ZerxesWolfHawk@users.noreply.github.com>
2019-03-19 16:29:25 -0700

    Add files via upload

---
+++ cosineSimilarity.js
@@ -0,0 +1,84 @@
+/*
+* INPUTS
+* docWords: String Array for the document being checked for plagiarism
+* compareWords: String Array for the document being compared to
+*
+* OUTPUTS
+* similarity: Number showing the similarity between the inputs
+*/
+
+function cosineSimilarity(docWords, compareWords)
+{
+	//Create Word Frequency lists
+	//list[word] = frequency
+	var docWordFrequency = new Object();
+	var compareWordFrequency = new Object();
+	
+	//Populate Word Frequency lists
+	for (sentence in docWords)
+	{
+		docWords[sentence] = docWords[sentence].split(" ");
+		for (word in docWords[sentence])
+		{
+			if (isNaN(docWordFrequency[docWords[word]]))
+			{
+				docWordFrequency[docWords[word]] = 1;
+			}
+			else
+			{
+				docWordFrequency[docWords[word]]++;
+			}
+		}
+	}
+
+	for (sentence in compareWords)
+	{
+		compareWords[sentence] = compareWords[sentence].split(" ");
+		for (word in compareWords[sentence])
+		{
+			if (isNaN(compareWordFrequency[compareWords[word]]))
+			{
+				compareWordFrequency[compareWords[word]] = 1;
+			}
+			else
+			{
+				compareWordFrequency[compareWords[word]]++;
+			}
+		}
+	}
+	
+	//Normalize Word Frequency lists
+	var normalizeDoc = 0;
+	for (word in docWordFrequency)
+	{
+		normalizeDoc += docWordFrequency[word]**2;
+	}
+	normalizeDoc = Math.sqrt(normalizeDoc);
+    for (word in docWordFrequency)
+    {
+    	docWordFrequency[word] = docWordFrequency[word] / normalizeDoc;
+    }
+    
+	var normalizeCompare = 0;
+	for (word in compareWordFrequency)
+	{
+		normalizeCompare += compareWordFrequency[word]**2;
+	}
+	normalizeCompare = Math.sqrt(normalizeCompare);
+    for (word in compareWordFrequency)
+    {
+    	compareWordFrequency[word] = compareWordFrequency[word] / normalizeCompare;
+    }
+    
+	//Multiply normalized frequencies, per word shared
+	var similarity = 0;
+	for (word in docWordFrequency)
+	{
+		if (compareWordFrequency[word] != undefined)
+		{
+			similarity += docWordFrequency[word] * compareWordFrequency[word];
+		}
+	}
+	
+	return similarity;
+}
---
+++ diceSimilarity.js
@@ -0,0 +1,74 @@
+/*
+* INPUTS
+* docWords: String Array of sentences for the document being checked for plagiarism
+* compareWords: String Array of sentences for the document being compared to
+*
+* OUTPUTS
+* similarity: Number showing the similarity between the inputs
+*/
+
+function diceSimilarity(docWords, compareWords)
+{
+	//Create Word Frequency lists
+	//list[word] = frequency
+	var docWordFrequency = new Object();
+	var compareWordFrequency = new Object();
+	
+	//Populate Word Frequency lists
+	for (sentence in docWords)
+	{
+		docWords[sentence] = docWords[sentence].split(" ");
+		for (word in docWords[sentence])
+		{
+			if (isNaN(docWordFrequency[docWords[sentence][word]]))
+			{
+				docWordFrequency[docWords[sentence][word]] = 1;
+			}
+			else
+			{
+				docWordFrequency[docWords[sentence][word]]++;
+			}
+		}
+	}
+
+	for (sentence in compareWords)
+	{
+		compareWords[sentence] = compareWords[sentence].split(" ");
+		for (word in compareWords[sentence])
+		{
+			if (isNaN(compareWordFrequency[compareWords[sentence][word]]))
+			{
+				compareWordFrequency[compareWords[sentence][word]] = 1;
+			}
+			else
+			{
+				compareWordFrequency[compareWords[sentence][word]]++;
+			}
+		}
+	}
+
+	//Count number of words that appear in both documents
+    //A word used more than once in a document is only counted the first time
+    var similarity = 0;
+    for (word in docWordFrequency)
+    {
+    	if (compareWordFrequency[word] != undefined)
+			{
+				similarity += 2;
+			}
+    }
+    
+    //Count total words used among both documents
+    var totalWords = 0;
+    for (word in docWordFrequency)
+    {
+    	totalWords += docWordFrequency[word];
+    }
+    for (word in compareWordFrequency)
+    {
+    	totalWords += compareWordFrequency[word];
+    }
+    
+    similarity /= totalWords;
+	return similarity;
+}
